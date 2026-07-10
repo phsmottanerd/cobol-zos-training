95e4df1f25287ba1f74be26044cd9bcfdba202ca
Paulo Henrique Santana Motta <166978623+phsmottanerd@users.noreply.github.com>
2026-07-10 17:31:58 -0300

    Update PROGRAMA02.cob

--- PROGRAMA02.cob
+++ PROGRAMA02.cob
@@ -74,6 +74,57 @@
 
        STOP RUN.
 
-       END PROGRAM HELLO.
-
+       
       
+       *===========================================
+      * Autor      : Paulo Henrique Santana Motta
+      * Empresa    : XXXXX
+      * Objetivo   : Primeiro contato com COBOL
+      * Data       : 09/08/2026
+      * Observações: Programa de teste
+      *===========================================
+      
+      
+      
+      
+      END PROGRAM HELLO.
+
+         >>SOURCE FORMAT FREE
+
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO.
+
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+
+       77 WRK-NOME         PIC X(30) VALUE SPACES.
+       77 WRK-IDADE        PIC 9(03) VALUE ZEROS.
+       77 WRK-SALARIO      PIC 9(08) VALUE ZEROS.
+
+       PROCEDURE DIVISION.
+
+       0100-RECEBE SECTION.
+
+           DISPLAY "DIGITE O SEU NOME: "
+           ACCEPT WRK-NOME
+
+           DISPLAY "DIGITE SUA IDADE: "
+           ACCEPT WRK-IDADE
+
+           DISPLAY "DIGITE SEU SALARIO: "
+           ACCEPT WRK-SALARIO
+
+           PERFORM 0200-MOSTRA.
+
+       0200-MOSTRA SECTION.
+
+           DISPLAY "=============================="
+           DISPLAY "NOME: " WRK-NOME
+           DISPLAY "IDADE: " WRK-IDADE
+           DISPLAY "SALARIO: " WRK-SALARIO
+           DISPLAY "=============================="
+
+           PERFORM 0300-FINALIZAR.
